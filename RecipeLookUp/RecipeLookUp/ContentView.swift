32c6841be009a6fcdbf64b0935ec8790211e86a1
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2024-03-17 18:53:15 +0100

    Proyect creation

---
+++ RecipeLookUp/RecipeLookUp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeLookUp
+//
+//  Created by José Luis Corral on 17/3/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ RecipeLookUp/RecipeLookUp/RecipeLookUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeLookUpApp.swift
+//  RecipeLookUp
+//
+//  Created by José Luis Corral on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeLookUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
