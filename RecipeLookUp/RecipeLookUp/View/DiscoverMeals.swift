4af6c78963dbe4c9d8fa5fc6fd32faa8e435c4a7
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2024-03-17 20:01:32 +0100

    Both initial views created

---
+++ RecipeLookUp/RecipeLookUp/View/DiscoverMeals.swift
@@ -0,0 +1,18 @@
+//
+//  DiscoverMeals.swift
+//  RecipeLookUp
+//
+//  Created by José Luis Corral on 17/3/24.
+//
+
+import SwiftUI
+
+struct DiscoverMeals: View {
+    var body: some View {
+        Text("Browse some new meals")
+    }
+}
+
+#Preview {
+    DiscoverMeals()
+}
---
+++ RecipeLookUp/RecipeLookUp/View/MyMeals.swift
@@ -0,0 +1,18 @@
+//
+//  MyMeals.swift
+//  RecipeLookUp
+//
+//  Created by José Luis Corral on 17/3/24.
+//
+
+import SwiftUI
+
+struct MyMeals: View {
+    var body: some View {
+        Text("Personal Page")
+    }
+}
+
+#Preview {
+    MyMeals()
+}
